af7fccc52829d1a1b9ae49d15c3ebe4e339ec6d8
cyyllam <cyyllam@gmail.com>
2024-12-14 00:50:01 -0800

    create draft of end table

--- others/theme-analysis/theme-analysis.R
+++ others/theme-analysis/theme-analysis.R
@@ -16,17 +16,46 @@ df_piv <- df |>
 df_piv2 <- df_piv |> 
   nest(list_of_juris = juris)
 
-# create all possible theme combinations in groups of 3
+# create all possible unique theme combinations in groups of 3
 themes <- unique(df_piv$theme)
-grid <- combinations(13, 3, themes) |> 
+grid <- combinations(length(themes), 3, themes) |> 
   as_tibble() |> 
   rowwise() |> 
-  mutate(combos = list(c(V1, V2, V3)))
+  mutate(combos = list(c(V1, V2, V3))) |> 
+  unite(combo_label, V1, V2, V3, sep = ", ", remove = FALSE)
 
-# for(i in 1:length(grid$combos)) {
-for(i in 1:5) {
+# initiate main table
+
+# loop through each combo
+for(i in 1:length(grid$combos)) {
+# for(i in 1:1) {
   print(grid$combos[[i]])
+  
+  # filter for combo
   df_f <- df_piv2 |> 
-    filter(theme %in% grid$combos[[i]])
+    filter(theme %in% grid$combos[[i]]) |> 
+    mutate(combo_label = paste(grid$combos[[i]], collapse = ", "))
   print(df_f)
+  
+  # unpack tibbles (list of jurisdictions per theme)
+  df_unnest <- df_f |> 
+    unnest(list_of_juris)
+  
+  df_summary <- df_unnest |> 
+    group_by(juris, combo_label) |>
+    summarise(num_themes = n()) |> 
+    arrange(desc(num_themes))
+
+  # find specific theme each city belongs to
+  juris_theme <- df_piv |> 
+    filter(theme %in% grid$combos[[i]]) |> 
+    nest(theme = theme) |> 
+    unnest_wider(theme)
+  
+  df_join <- df_summary |> 
+    left_join(juris_theme, by = 'juris')
+  
+  # append to main table
 }
+
+
