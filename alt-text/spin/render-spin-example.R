26d5d835cdf393d458630b974e357960e0ca30ff
cyyllam <cyyllam@gmail.com>
2026-03-17 15:05:17 -0700

    examples of using children

---
+++ alt-text/spin/render-spin-example.R
@@ -0,0 +1,4 @@
+rmarkdown::render(here::here("alt-text", "spin", "spin-example.R"))
+
+# http://brooksandrew.github.io/simpleblog/articles/render-reports-directly-from-R-scripts/
+# https://yihui.org/rmarkdown-cookbook/source-script
---
+++ alt-text/spin/spin-example.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Crop Analysis Q3 2013"
+#' author: "John Smith"
+#' date: "May 3rd, 2014"
+#' ---
+#' A script comment that includes **markdown** formatting.
+#' 
+#' https://rmarkdown.rstudio.com/articles_report_from_r_script.html
+#' 
+#+ fig.width=5, fig.height=5
+plot(cars)
